029fd03c53fadf36555abecaff8a0501aedd4d35
gal libenson <galli@ceragon.com>
2024-07-23 15:48:39 +0300

    Create ToDoList.py

---
+++ ToDO_List/ToDoList.py
@@ -0,0 +1,45 @@
+
+from collections.abc import ItemsView
+toDo =[]  
+
+def view(t) :
+  for i in toDo :
+    print(i) 
+
+def add(list,item):
+  if check(item)==True:
+    list.append(item)
+
+def delete(list,item):
+  if check(item)==True and item in list:
+    list.remove(item.index(item))
+
+def check(item):
+  if type(item) != str:
+    print("invalid input")
+    return False 
+  else:
+    return True 
+
+
+
+while True :
+  print("1. view")
+  print("2. add")
+  print("3. delete")
+  print("4. exit")
+  c = int(input("what action you want to take? \n"))
+
+  if c==1:
+    view(toDo)
+  elif c==2:
+    add(toDo,input("enter item"))
+  elif c==3:
+    delete(toDo,item) 
+  else :
+    print("exit")
+    break
+
+
+
+
