da0e5c0cfd8cd9c50cab33b7d2c2bdf112bd8614
gal libenson <galli@ceragon.com>
2024-07-28 18:00:37 +0300

    Update diceRolling.py

--- DICE_ROLLING/diceRolling.py
+++ DICE_ROLLING/diceRolling.py
@@ -1,12 +1,23 @@
-import random 
-def display(roll1):
+import random
+
+def display(roll1,count):
   print("result : "+str(roll1))
+  print("counter = "+ str(count+1))
 
 def roll():
-      roll = random.randint(1,6)
-      display(roll)
-      return roll 
-        
+    global total,count
+    roll = random.randint(1,6)
+    display(roll,count)
+    total += roll
+    count += 1
+    return roll
+
+total =0
+count =0
+
+
+
+
 i = int(input("how many times you want to roll the dice?"))
 if  i < 0 and i > 6:
   print("invalid input")
