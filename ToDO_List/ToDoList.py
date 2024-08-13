18153e8c7adee4114a0b8daab753f067b5450fae
gal libenson <galli@ceragon.com>
2024-08-13 16:36:41 +0300

    improving existed functions

--- ToDO_List/ToDoList.py
+++ ToDO_List/ToDoList.py
@@ -1,4 +1,4 @@
-import 
+ 
 from collections.abc import ItemsView
 toDo =[] 
 class mission : 
@@ -8,56 +8,35 @@ class mission :
     self.finishedDate = finishedDate
 
 
-
-def view(mission) :
-  for i in toDo :
-    print(i) 
-
-def add(list,mission):
-  if check(mission)==True:
-    list.append(mission)
-
-def delete(list,mission):
-  if check(mission)==True and mission in list:
-    list.remove(item.index(mission))
-
-def check(item):
+def helpFind(item):
   if type(item) != str:
     print("invalid input")
     return False 
   else:
     return True 
 
+def viewTask(mission) :
+  for i in toDo :
+    print(i) 
 
+def addTask(list,mission):
+  if helpFind(mission)==True:
+    list.append(mission)
 
-while True :
-  print("1. view")
-  print("2. add")
-  print("3. delete")
-  print("4. exit")
-  c = int(input("what action you want to take? \n"))
-
-  if c==1:
-    view(toDo)
-  elif c==2:
-    add(toDo,input("enter item"))
-  elif c==3:
-    delete(toDo,item) 
-  else :
-    print("exit")
-    break
-
+def deleteTask(list,mission):
+  if helpFind(mission)==True and mission in list:
+    list.remove(item.index(mission))
 
 def printobj(mission):
-  print(mission.task)
-  print(mission.introDate)
-  print(mission.finishedDate)
+  print(mission.task,'\n')
+  print(mission.introDate,'\n')
+  print(mission.finishedDate,'\n')
 
 def checkTimeLeft(toDo):
   for i in toDo:
     if toDo[i].finishedDate<5 :
-       printobj(toDo[i])
-       print("need to finish this task ")
+       print(f"need to finish this task ASAP: \n
+       {printobj(toDo[i])} ")
 
 
 
@@ -65,3 +44,24 @@ while True:
     checkTimeLeft(toDo)
     time.sleep(604800)
     
+
+
+
+# while True :
+#   print("1. view")
+#   print("2. add")
+#   print("3. delete")
+#   print("4. exit")
+#   c = int(input("what action you want to take? \n"))
+
+#   if c==1:
+#     view(toDo)
+#   elif c==2:
+#     add(toDo,input("enter item"))
+#   elif c==3:
+#     delete(toDo,item) 
+#   else :
+#     print("exit")
+#     break
+
+
