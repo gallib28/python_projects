235ab7d0fe8b70154d834aa0be465fc355a92acc
gal libenson <galli@ceragon.com>
2024-08-18 13:45:44 +0300

    on

--- ToDO_List/ToDoList.py
+++ ToDO_List/ToDoList.py
@@ -17,7 +17,18 @@ class Mission :
     self.name = task
     self.introDate = introDate
     self.dueDate = finishedDate
+>>>>>>> 9832751b43050eadffd820c03c45ee10e4240d93
 
+<<<<<<< HEAD
+# class Mission : 
+#   name="name of task"
+#   dueDate = "00-00-0000"
+#   createDate = "00-00-0000" 
+#   def __init__(self, task ,introDate,finishedDate):
+#     self.name = task
+#     self.introDate = introDate
+#     self.dueDate = finishedDate
+=======
   def getDueDate():
     return this.dueDate 
   
@@ -28,58 +39,66 @@ def helpFind(item):
     return False 
   else:
     return True 
+>>>>>>> 9832751b43050eadffd820c03c45ee10e4240d93
 
-def viewTask(mission) :
-  for i in toDo :
-    print(i) 
+# def helpFind(item):
+#   if type(item) != str:
+#     print("invalid input")
+#     return False 
+#   else:
+#     return True 
 
-def addTask(list,mission):
-  if helpFind(mission)==True:
-    list.append(mission)
-    size += 1 
+# def viewTask(mission) :
+#   for i in toDo :
+#     print(i) 
 
-def deleteTask(list,mission):
-  if helpFind(mission)==True and mission in list:
-    list.remove(item.index(mission))
-    size -= 1 
+# def addTask(list,mission):
+#   if helpFind(mission)==True:
+#     list.append(mission)
+#     size += 1 
 
-
-def printobj(mission):
-  print(mission.name,'\n')
-  print(mission.introDate,'\n')
-  print(mission.finishedDate,'\n')
-
-
-def checkTimeLeft(toDo):
-  for i in toDo:
-    if toDo[i].finishedDate<5 :
-       print(f"need to finish this task ASAP: \n
-       {printobj(toDo[i])} ")
+# def deleteTask(list,mission):
+#   if helpFind(mission)==True and mission in list:
+#     list.remove(item.index(mission))
+#     size -= 1 
 
 
-# for timing the program 
-while True:
-    checkTimeLeft(toDo)
-    time.sleep(604800)
+# def printobj(mission):
+#   print(mission.name,'\n')
+#   print(mission.introDate,'\n')
+#   print(mission.finishedDate,'\n')
+
+
+# def checkTimeLeft(toDo):
+#   for i in toDo:
+#     if toDo[i].finishedDate<5 :
+#        print(f"need to finish this task ASAP: \n
+#        {printobj(toDo[i])} ")
+
+
+# # for timing the program 
+# while True:
+#     checkTimeLeft(toDo)
+#     time.sleep(604800)
     
 
-toDo = [] 
-size = 0 
-# while True :
-#   print("1. view")
-#   print("2. add")
-#   print("3. delete")
-#   print("4. exit")
-#   c = int(input("what action you want to take? \n"))
+# toDo = [] 
+# size = 0 
+# # while True :
+# #   print("1. view")
+# #   print("2. add")
+# #   print("3. delete")
+# #   print("4. exit")
+# #   c = int(input("what action you want to take? \n"))
 
-#   if c==1:
-#     view(toDo)
-#   elif c==2:
-#     add(toDo,input("enter item"))
-#   elif c==3:
-#     delete(toDo,item) 
-#   else :
-#     print("exit")
-#     break
+# #   if c==1:
+# #     view(toDo)
+# #   elif c==2:
+# #     add(toDo,input("enter item"))
+# #   elif c==3:
+# #     delete(toDo,item) 
+# #   else :
+# #     print("exit")
+# #     break
 
 
